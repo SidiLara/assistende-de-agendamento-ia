d8aba4fe229cc89be24a7da89e2205c859715ad3
Sidinei <91575733+SidiLara@users.noreply.github.com>
2025-10-29 15:26:09 -0300

    refactor: Remove React.StrictMode and duplicate CSS import
    The React.StrictMode wrapper was removed from the root `ReactDOM.createRoot` call. Additionally, the import of `index.css` was moved from `src/pages/batePapo/BatePapoPagina.tsx` to `index.tsx` to ensure it's only imported once globally. This simplifies the application's entry point and prevents potential styling conflicts.

--- src/pages/batePapo/BatePapoPagina.tsx
+++ src/pages/batePapo/BatePapoPagina.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import '../../index.css';
 import { CabecalhoDoChat } from '../../component/CabecalhoDoChat';
 import { CorpoDoChat } from '../../component/CorpoDoChat';
 import { EntradaDeChat } from '../../component/EntradaDeChat';
