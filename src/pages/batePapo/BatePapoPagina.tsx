2037331078b146f80faeffb90886333ec08d21e0
Sidinei <91575733+SidiLara@users.noreply.github.com>
2025-10-29 15:01:43 -0300

    feat: Import global styles into chat page
    Moves the import of global CSS styles from `index.tsx` to `BatePapoPagina.tsx` to ensure styles are correctly applied to the chat interface.

--- src/pages/batePapo/BatePapoPagina.tsx
+++ src/pages/batePapo/BatePapoPagina.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '../../index.css';
 import { CabecalhoDoChat } from '../../component/CabecalhoDoChat';
 import { CorpoDoChat } from '../../component/CorpoDoChat';
 import { EntradaDeChat } from '../../component/EntradaDeChat';
